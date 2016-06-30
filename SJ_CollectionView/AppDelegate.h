8e1406f63d647b4b4c922855bd0b93979e561a07
fushengjun <fushengjun@xiangshang360.com>
2016-06-30 17:24:30 +0800

    Initial Commit

---
+++ SJ_CollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SJ_CollectionView
+//
+//  Created by SPIREJ on 16/6/30.
+//  Copyright © 2016年 SPIREJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
